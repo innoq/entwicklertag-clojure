02d79c39fd04f47df6f653683749f4d1bde37b05
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2015-02-11 08:14:47 +0100

    removed unused dependency on hiccup

--- minimal-webapp/project.clj
+++ minimal-webapp/project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.1"]
-                 [compojure "1.1.9"]
-                 [hiccup "1.0.5"]])
+                 [compojure "1.1.9"]])
